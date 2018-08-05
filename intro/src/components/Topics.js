698b96b95ad2e257f3fcc95ebde96654e4cbde3e
rio <riogoodman@gmail.com>
2018-08-05 09:57:21 -0700

    react router, dynamic routing

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import {Link, Route} from 'react-router-dom'
 
-export default function Topics() {
-  return <div>TOPICS</div>
+function Topic(props) {
+  console.log('props to topci', props)
+  return <h3>{props.match.params.topicId}</h3>
+}
+
+export default function Topics(props) {
+  console.log('props to topcis', props)
+  return (
+    <div>
+      <h2>Topcis</h2>
+      <ul>
+        <li>
+          <Link to={`${props.match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to='/topics/components'>React Components</Link>
+        </li>
+        <li>
+          <Link to='/topics/props-v-state'>Props vs. State</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path={`${props.match.path}/:topicId`} component={Topic} />
+    </div>
+  )
 }
